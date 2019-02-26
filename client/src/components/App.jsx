bd07063b4560ada84fe09349e6b55eaeaffaf05a
akwong372 <akwong372@gmail.com>
2019-02-25 23:50:53 -0800

    Sort reviews by tags

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,29 +12,55 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      reviews: [{ "id": 1, "userId": 42, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-21", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Cellar, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
-      "user":{"id":38,"username":"MikeS","location":"Seattle","vip":"Yes", "reviewNumber":10}},
-      { "id": 2, "userId": 34, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-22", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Cellar, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
-      "user":{"id":34,"username":"MikeR","location":"Seattle","vip":"No", "reviewNumber":7}}],
-      filteredReviews: [{ "id": 1, "userId": 42, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-21", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Cellar, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
-      "user":{"id":38,"username":"MikeS","location":"Seattle","vip":"Yes", "reviewNumber":10}},
-      { "id": 2, "userId": 34, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-22", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Cellar, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
-      "user":{"id":34,"username":"MikeR","location":"Seattle","vip":"No", "reviewNumber":7}}],
-      popularTags: ['Organic', 'Quick Booty'],
+      reviews: [{
+        "id": 1, "userId": 42, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-21", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Cellar, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
+        "user": { "id": 38, "username": "MikeS", "location": "Seattle", "vip": "Yes", "reviewNumber": 10 }
+      },
+      {
+        "id": 2, "userId": 34, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-22", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
+        "user": { "id": 34, "username": "MikeR", "location": "Seattle", "vip": "No", "reviewNumber": 7 }
+      }],
+      filteredReviews: [{
+        "id": 1, "userId": 42, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-21", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Cellar, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
+        "user": { "id": 38, "username": "MikeS", "location": "Seattle", "vip": "Yes", "reviewNumber": 10 }
+      },
+      {
+        "id": 2, "userId": 34, "restaurantId": "thfUjkN4Tv", "reviewDate": "2019-02-22", "text": "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.", "tags": "Vibrant Bar Scene, Organic, Quick Bite, Open Kitchen, Couples, Vegan", "overallScore": 4, "foodScore": 3, "serviceScore": 3, "ambienceScore": 5, "valueScore": 4, "noise": 1, "recommend": "Y", "createdAt": "2019-02-15T20:26:17.000Z", "updatedAt": "2019-02-15T20:26:17.000Z",
+        "user": { "id": 34, "username": "MikeR", "location": "Seattle", "vip": "No", "reviewNumber": 7 }
+      }],
+      popularTags: ['Organic', 'Cellar', 'Not Valid'],
       checkedTags: []
     };
     this.filterByTag = this.filterByTag.bind(this);
   }
 
-  filterByTag(e){
-    if (e.target.checked){
+  filterByTag(e) {
+    if (e.target.checked) {
+      // console.log(e.target.checked)
       this.setState({
-        checkedTags: this.state.checkedTags.concat([e.target.labels[0].id]),
-        filteredReviews: this.state.reviews.filter((review)=> {
-          return this.state.checkedTags.every((tag)=> review.tags.indexOf(tag) > -1);
+        checkedTags: this.state.checkedTags.concat([e.target.labels[0].id])
+      });
+    } else if (e.target.checked === false) {
+      var removeTag = this.state.checkedTags.slice();
+      removeTag.splice(removeTag.indexOf(e.target.labels[0].id), 1);
+      // console.log(e.target.labels[0].id)
+      this.setState({
+        checkedTags: removeTag
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    // console.log('previous: ', prevState.checkedTags)
+    // console.log('current: ', this.state.checkedTags)
+    if (this.state.checkedTags.length !== prevState.checkedTags.length) {
+      this.setState({
+        filteredReviews: this.state.reviews.filter((review) => {
+          return this.state.checkedTags.every((tag) => review.tags.indexOf(tag) >= 0);
         })
-      }, ()=>{console.log(this.state.filteredReviews)});
-    } 
+      });
+    }
   }
 
   render() {
@@ -52,7 +78,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>test</h1>
-        <TagsSorting tagList={this.state.popularTags} filterByTag={this.filterByTag} reviewNumber={this.state.reviews}/>
+        <TagsSorting tagList={this.state.popularTags} filterByTag={this.filterByTag} reviewNumber={this.state.filteredReviews} />
         {mappedReviews}
       </div>
     );
--- client/src/components/TagsSorting.jsx
+++ client/src/components/TagsSorting.jsx
@@ -8,9 +8,9 @@ const TagsSorting = (props) => {
   if (tagList.length > 0) {
     filtersSection = tagList.map((tag, i) =>
       <span key={'filterTag' + i} className="filterTag">
-        <input type="checkbox" id={'reviewFilter' + i} onClick={(e)=>{e.persist(); props.filterByTag(e)}}/>
+        <input type="checkbox" id={'reviewFilter' + i} onClick={(e) => { e.persist(); props.filterByTag(e) }} />
         <label id={tag} htmlFor={'reviewFilter' + i}>
-          <span className="filterTagOption">{tag + 'word'}</span>
+          <span className="filterTagOption">{`${tag} (${props.reviewNumber.filter((review) => review.tags.includes(tag)).length})`}</span>
         </label>
       </span>
     );
@@ -20,10 +20,19 @@ const TagsSorting = (props) => {
     <div className="tagsSorting">
       <div className="dropdownLabel">Sort by</div>
       <div className="dropdownMenu">
+        <select name="dropdownSorting" id="dropdownSorting">
+        {/* <div>
+          <input type="radio" name="newest" id="newest"/>
+          <label htmlFor="newest">Newest</label>
+        </div> */}
+          <option value="Newest">Newest</option>
+          <option value="Highest rating">Highest rating</option>
+          <option value="Lowest rating">Lowest rating</option>
+        </select>
       </div>
       <div className="filtersLabel">Filters</div>
       <div className="filtersSection">
-      {filtersSection}
+        {filtersSection}
       </div>
     </div>
   )
