f13f1056e8bd9be57f228442e6611a99f31705c7
akwong372 <akwong372@gmail.com>
2019-03-08 12:33:26 -0800

    Add morgan

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
 const path = require('path');
 const db = require('./db.js');
 
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public/dist')));
 app.use('/restaurants/:restaurantId/reviews', express.static(path.join(__dirname, '../public/dist')));
